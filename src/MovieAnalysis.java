f31d8fed2ad74a45a24d7da801b28d897ee43a76
Maria J Kirkou <mariajamalaki@hotmail.com>
2025-02-20 15:52:39 +0100

    test Högre Funktioner

--- src/MovieAnalysis.java
+++ src/MovieAnalysis.java
@@ -1,5 +1,8 @@
 import java.util.List;
 
+// T är typen på datan vi ska analysera Movies
+//R är typen på resultatet vi vill ha Long/String
+
 @FunctionalInterface
 public interface MovieAnalysis<T, R> {
     R analysera (List<T> data); //lista med saker av typ T
--- src/MovieStatistics.java
+++ src/MovieStatistics.java
@@ -1,9 +1,25 @@
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MovieStatistics {
     //detta är själva hjälp klassen för alla analysfunktioner vi utför med movieanalysis
 
     public static <T, R> R utförAnalys(List<T> data, MovieAnalysis<T, R> analysFunktion) {
-        return analysFunktion.analysera(data);
+        return analysFunktion.analysera(data);  // tar en annan funktion som parameter
     }
+
+    public static final MovieAnalysis<Movie, Optional<Movie>> findTheLongestMovie =
+            movies -> movies.stream()
+                    .max(Comparator.comparingInt(Movie::getRuntime));
+
+    public static final MovieAnalysis<Movie, Optional<String>> findTheMostPopularActor =
+            movies -> movies.stream().flatMap(movie -> movie.getCast().stream()).collect(Collectors.groupingBy(
+                    actor -> actor,Collectors.counting())).entrySet().stream().max(Map.Entry.comparingByValue())
+                    .map(Map.Entry::getKey);
+
+
 }
+
